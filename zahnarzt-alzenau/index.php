d71cd357615d178737ea3a3df7a2a7690f69ee06
Marco Debus <marco@debus-software-ug.de>
2020-03-30 15:34:55 +0200

    bf

--- zahnarzt-alzenau/index.php
+++ zahnarzt-alzenau/index.php
@@ -243,7 +243,7 @@ include('./inc/header.php');
 						  <div class="modal-dialog" role="document">
 						    <div class="modal-content">
 						      <div class="modal-header">
-						        <h5 class="modal-title" id="exampleModalLabel"><h4>Corona Pandemie</h4> <br> Erhöhte Sicherheitsmaßnahmen</h5>
+						        <h5 class="modal-title" id="exampleModalLabel"><h4>Corona Pandemie</h4> <br> <h5>Erhöhte Sicherheitsmaßnahmen</h5>
 
 						        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
 						          <span aria-hidden="true">&times;</span>
