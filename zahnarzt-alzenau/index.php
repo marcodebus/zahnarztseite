771613b2495def420f8473130a4f476860be8ebd
Marco Debus <33735167+marcodebus@users.noreply.github.com>
2020-07-08 17:28:07 +0200

    Update index.php

--- zahnarzt-alzenau/index.php
+++ zahnarzt-alzenau/index.php
@@ -175,7 +175,8 @@ include('./inc/header.php');
 												echo"</td><td>";
 												 echo lang($array[$i].'1');
 												//echo"</td><td>";
-												if (lang($array[$i].'2' !== ''){
+												$var = lang($array[$i].'2';
+												if($var !== ''){
 													echo lang('und');
 													//echo"</td><td>";
 													 echo lang($array[$i].'2');
