ef3f8f0c806b4109ba06d214f97a98f3e5996fdc
Marco Debus <33735167+marcodebus@users.noreply.github.com>
2020-07-08 22:28:38 +0200

    Update oeffnungszeiten.php

--- zahnarzt-alzenau/oeffnungszeiten.php
+++ zahnarzt-alzenau/oeffnungszeiten.php
@@ -121,9 +121,10 @@ include('./inc/headerSlider.php');
                             </div>
 
 
-              <div class="col-lg-12  .color-9" style="background-color:#4590e7; padding-top: 8px; padding-bottom: 1px; margin-bottom: 10px; border-radius:20px;">
-
-      					   		<p align="center" style="color:white;">
+               <!--
+					<div class="col-lg-12  .color-9" style="background-color:#4590e7; padding-top: 8px; padding-bottom: 1px; margin-bottom: 10px; border-radius:20px;">
+-->
+      					   		<br><p align="center" style="color:white;">
       						   		<?php echo lang('OeffnungszeitenText');?>
                       </p>
       				</div>
