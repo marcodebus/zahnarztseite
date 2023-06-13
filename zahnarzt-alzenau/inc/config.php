8e4cfa18f89dad15cf17a89b5d6805b73b655dc0
Mia Seemann <115574001+miaseemann@users.noreply.github.com>
2023-06-13 08:19:31 +0200

    neues teammitglied

--- zahnarzt-alzenau/inc/config.php
+++ zahnarzt-alzenau/inc/config.php
@@ -328,7 +328,7 @@ function lang($phrase){
     'TeamBildAn2' => '../wp-content/uploads/2019/01/zahnarzt_fuchs_alzenau_rauch-1600x1600.png',
 		'TeamBildAn3' => '../wp-content/uploads/2019/01/zahnarzt_fuchs_alzenau_jaxt-1600x1600.png',
     'TeamBildAn4' => '../wp-content/uploads/2019/01/zahnarzt_fuchs_alzenau_arzu-1600x1600.png',
-    'TeamBildAn5' => '../zahnarzt-alzenau/assets/bilder/teambilder/avatar.png',
+    'TeamBildAn5' => '../zahnarzt-alzenau/assets/bilder/teambilder/avatar-frau.png',
 
 
     'TeamMitgliedUeber1' => 'Dr. med. dent. Bernhard Fuchs',
